f27ad0d126f72b88290cf7be34aae66ba19c2210
BattleRush <karlo.piskor1995@gmail.com>
2020-11-08 22:29:13 +0100

    Initial commit

---
+++ ETHDINFKBot/DiscordModule.cs
@@ -0,0 +1,105 @@
+﻿using Discord;
+using Discord.Addons.Interactive;
+using Discord.Commands;
+using Discord.WebSocket;
+using DuckSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETHDINFKBot
+{
+    public class DiscordModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("code")]
+        public async Task SourceCode()
+        {
+            Context.Channel.SendMessageAsync("", false);
+        }
+
+        [Command("duck")]
+        [Summary("Returns info about the current user, or the user parameter, if one passed.")]
+        public async Task DuckDuckGo(string searchString,
+        [Summary("The (optional) user to get info from")]
+        SocketUser user = null)
+        {
+            var userInfo = user ?? Context.Message.Author;
+            //await ReplyAsync($"{userInfo.Username}#{userInfo.Discriminator}");
+
+            var reply = await new DuckSharpClient().GetInstantAnswerAsync(searchString);
+
+
+
+            EmbedBuilder builder = new EmbedBuilder();
+
+            builder.WithTitle("Search for " + searchString);
+            builder.WithDescription(reply.AbstractText + Environment.NewLine + reply.AbstractUrl);
+            builder.WithColor(255, 0, 0);
+
+            if (reply?.RelatedTopics != null && reply.RelatedTopics.Length > 0)
+            {
+                builder.Description = reply.RelatedTopics[0].Text;
+            }
+
+            if (reply.ImageUrl != null && reply.ImageUrl.Length > 0)
+            {
+                builder.WithThumbnailUrl($"https://duckduckgo.com{reply.ImageUrl}");
+            }
+
+            builder.WithAuthor(userInfo);
+            builder.WithFooter($"{userInfo.Username}#{userInfo.Discriminator}");
+            builder.WithCurrentTimestamp();
+
+            // TODO Error handling
+            foreach (var item in reply.RelatedTopics.Select(i => i).Take(4))
+            {
+                builder.AddField(item.Text, item.FirstUrl);
+            }
+
+            Context.Channel.SendMessageAsync("", false, builder.Build());
+        }
+
+
+        [Command("google")]
+        [Summary("Returns info about the current user, or the user parameter, if one passed.")]
+        public async Task GoogleSearch(string searchString,
+        [Summary("The (optional) user to get info from")]
+        SocketUser user = null)
+        {
+            var userInfo = user ?? Context.Message.Author;
+            //await ReplyAsync($"{userInfo.Username}#{userInfo.Discriminator}");
+
+            var reply = new Engine().Search(searchString);
+
+
+
+            EmbedBuilder builder = new EmbedBuilder();
+
+            builder.WithTitle("Search for " + searchString);
+            builder.WithDescription(reply.Description?.Substring(0, reply.Description.Length > 100 ? 100 : reply.Description.Length));
+            builder.WithColor(255, 0, 0);
+
+            //builder.Description = reply.RelatedTopics[0].Text;
+            if (reply.ImageUrl != null)
+            {
+                //builder.WithThumbnailUrl(reply.ImageUrl);
+            }
+
+            builder.WithAuthor(userInfo);
+            builder.WithFooter($"{userInfo.Username}#{userInfo.Discriminator}");
+            builder.WithCurrentTimestamp();
+
+            foreach (var item in reply.Results.Take(3))
+            {
+                builder.AddField(item.title, item.description + Environment.NewLine + item.url);
+            }
+
+            Context.Channel.SendMessageAsync("", false, builder.Build());
+        }
+
+
+
+    }
+}
---
+++ ETHDINFKBot/Program.cs
@@ -0,0 +1,91 @@
+﻿using Discord;
+using Discord.Addons.Interactive;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using DuckSharp;
+
+namespace ETHDINFKBot
+{
+    class Program
+    {
+        private DiscordSocketClient Client;
+        private CommandService commands;
+
+        private IServiceProvider services;
+        public static IConfiguration Configuration;
+        public static string DiscordToken { get; set; }
+
+        static void Main(string[] args)
+        {
+            Configuration = new ConfigurationBuilder()
+              .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+              .Build();
+
+            DiscordToken = Configuration["DiscordToken"];
+
+            new Program().MainAsync(DiscordToken).GetAwaiter().GetResult();
+
+        }
+
+        public async Task MainAsync(string token)
+        {
+
+
+
+            Client = new DiscordSocketClient();
+
+            Client.MessageReceived += HandleCommandAsync;
+
+
+
+            await Client.LoginAsync(TokenType.Bot, token);
+            await Client.StartAsync();
+
+#if DEBUG
+            await Client.SetGameAsync($"DEV MODE");
+#else
+            await Client.SetGameAsync($"DiskMath");
+#endif
+
+            services = new ServiceCollection()
+                .AddSingleton(Client)
+                .AddSingleton<InteractiveService>()
+                .BuildServiceProvider();
+
+            commands = new CommandService();
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+
+        public async Task HandleCommandAsync(SocketMessage m)
+        {
+            if (!(m is SocketUserMessage msg)) return;
+
+            int argPos = 0;
+            if (!(msg.HasStringPrefix(".", ref argPos)))
+            {
+                return;
+            }
+
+            var context = new SocketCommandContext(Client, msg);
+            await commands.ExecuteAsync(context, argPos, services);
+        }
+
+        private async static void Test()
+        {
+            
+        }
+    }
+}
