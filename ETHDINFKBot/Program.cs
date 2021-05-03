03505b67417398cedc4d7aef61a0cf63b15f5167
kpiskor <kpiskor@student.ethz.ch>
2021-05-03 21:57:47 +0200

    Change websocket url for nginx config

--- ETHDINFKBot/Program.cs
+++ ETHDINFKBot/Program.cs
@@ -224,16 +224,16 @@ namespace ETHDINFKBot
 #else*/
             PlaceWebsocket = new WebSocketServer(9000, true);
             PlaceWebsocket.SslConfiguration.EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12 | System.Security.Authentication.SslProtocols.Tls13;
-            PlaceWebsocket.SslConfiguration.ClientCertificateValidationCallback =
+            /*PlaceWebsocket.SslConfiguration.ClientCertificateValidationCallback =
               (sender, certificate, chain, sslPolicyErrors) => {
                   // Do something to validate the server certificate.
      
 
                 return true; // If the server certificate is valid.
-              };
+              };*/
             var cert = X509Certificate2.CreateFromPemFile(Path.Combine(Configuration["CertFilePath"], "cert.pem"), Path.Combine(Configuration["CertFilePath"], "privkey.pem"));
-            //PlaceWebsocket.SslConfiguration.ServerCertificate = cert;
-            PlaceWebsocket.AddWebSocketService<PlaceWebsocket>("/place");
+            PlaceWebsocket.SslConfiguration.ServerCertificate = cert;
+            PlaceWebsocket.AddWebSocketService<PlaceWebsocket>("/ws_place");
 
             PlaceWebsocket.Log.Level = WebSocketSharp.LogLevel.Debug;
             PlaceWebsocket.Log.File = Path.Combine(BasePath, "Log", "WebsocketLog.txt");
