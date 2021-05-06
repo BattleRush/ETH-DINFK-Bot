fc7d82b8f5ed1f5ba33eaf9ab9ae24f3b02a3391
kpiskor <kpiskor@student.ethz.ch>
2021-05-06 20:07:35 +0200

    Remove unused code and change port to 9000

--- ETHDINFKBot/Program.cs
+++ ETHDINFKBot/Program.cs
@@ -41,46 +41,6 @@ using System.Net.Sockets;
 namespace ETHDINFKBot
 {
 
-    class ChatSession : WssSession
-    {
-        public ChatSession(WssServer server) : base(server) { }
-
-        public override void OnWsConnected(HttpRequest request)
-        {
-            Console.WriteLine($"Chat WebSocket session with Id {Id} connected!");
-
-            // Send invite message
-            string message = "Hello from WebSocket chat! Please send a message or '!' to disconnect the client!";
-            SendTextAsync(message);
-        }
-
-        public override void OnWsDisconnected()
-        {
-            Console.WriteLine($"Chat WebSocket session with Id {Id} disconnected!");
-        }
-
-        public override void OnWsReceived(byte[] buffer, long offset, long size)
-        {
-            string message = Encoding.UTF8.GetString(buffer, (int)offset, (int)size);
-            Console.WriteLine("Incoming: " + message);
-
-            // Multicast message to all connected sessions
-            ((WssServer)Server).MulticastText(message);
-
-
-            /*((WssServer)Server).MulticastBinary(message);*/
-
-            // If the buffer starts with '!' the disconnect the current session
-            if (message == "!")
-                Close(1000);
-        }
-
-        protected override void OnError(SocketError error)
-        {
-            Console.WriteLine($"Chat WebSocket session caught an error with code {error}");
-        }
-    }
-
     class PlaceServer : WssServer
     {
         public PlaceServer(SslContext context, IPAddress address, int port) : base(context, address, port) { }
@@ -317,7 +277,7 @@ namespace ETHDINFKBot
             var context = new SslContext(SslProtocols.Tls12, new X509Certificate2(Path.Combine(Configuration["CertFilePath"], "battlerush.dev.pfx")));
 
             // Create a new WebSocket server
-            PlaceServer = new PlaceServer(context, IPAddress.Any, 9001);
+            PlaceServer = new PlaceServer(context, IPAddress.Any, 9000);
             PlaceServer.AddStaticContent(www, "/place");
 
             // Start the server
