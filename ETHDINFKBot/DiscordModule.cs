0dc15479d180c2b3e3b96fbc06dfc72577cc578f
Karlo <karlo.piskor1995@gmail.com>
2022-03-29 16:37:55 +0200

    Fix build

--- ETHDINFKBot/DiscordModule.cs
+++ ETHDINFKBot/DiscordModule.cs
@@ -1384,12 +1384,12 @@ Help is in EBNF form, so I hope for you all reading this actually paid attention
         [Command("lukas")]
         public async Task Lukas()
         {
-            if(LastLukasPing.AddMinutes(1)) > DateTime.Now)
+            if(LastLukasPing.AddMinutes(1) > DateTime.Now)
                 return;
 
             LastLukasPing = DateTime.Now;
-            await Context.Channel.SendMessageAsync($"<@223932775474921472>");
-            await Context.Channel.SendMessageAsync($"https://media.discordapp.net/attachments/768600365602963496/958082710100901988/ezgif.com-gif-maker.gif");
+            await Context.Channel.SendMessageAsync("<@223932775474921472>");
+            await Context.Channel.SendMessageAsync("https://media.discordapp.net/attachments/768600365602963496/958082710100901988/ezgif.com-gif-maker.gif");
         }
 
         [Command("food")]
