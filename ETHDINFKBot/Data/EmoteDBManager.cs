e1b3e039ec8862487d5869a1ff0513d92d99cdf6
Karlo Piskor <karlo.piskor1995@gmail.com>
2022-10-03 23:50:08 +0200

    Increase timeout for emote count from 1 to 3 sec

--- ETHDINFKBot/Data/EmoteDBManager.cs
+++ ETHDINFKBot/Data/EmoteDBManager.cs
@@ -149,7 +149,7 @@ namespace ETHDINFKBot.Data
                 {
                     using (var command = new MySqlCommand(sqlSelect, connection))
                     {
-                        command.CommandTimeout = 1;
+                        command.CommandTimeout = 3;
                         connection.Open();
 
                         return (long)command.ExecuteScalar();
