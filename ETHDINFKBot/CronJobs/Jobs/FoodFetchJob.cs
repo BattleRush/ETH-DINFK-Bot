ad44fdab3d93a1f3056b6a7acfd14ab32d2aa2a7
Karlo Piskor <karlo.piskor1995@gmail.com>
2023-08-31 07:49:37 +0200

    too lazy to debug this

--- ETHDINFKBot/CronJobs/Jobs/FoodFetchJob.cs
+++ ETHDINFKBot/CronJobs/Jobs/FoodFetchJob.cs
@@ -31,19 +31,26 @@ namespace ETHDINFKBot.CronJobs.Jobs
 
         public override Task DoWork(CancellationToken cancellationToken)
         {
+            var guild = Program.Client.GetGuild(Program.ApplicationSetting.BaseGuild);
+            var spamChannel = guild.GetTextChannel(GeneralChatId);
+
             _logger.LogInformation($"{DateTime.Now:hh:mm:ss} {Name} is working.");
             Console.WriteLine("Run FoodFetchJob");
 
-            // TODO Maybe send update message if the fetch was successfull
-            Stopwatch watch = new Stopwatch();
-            watch.Start();
-            var foodHelper = new FoodHelper();
-            foodHelper.LoadMenus(-1, false);
-            watch.Stop();
+            try
+            {
+                // TODO Maybe send update message if the fetch was successfull
+                Stopwatch watch = new Stopwatch();
+                watch.Start();
+                var foodHelper = new FoodHelper();
+                foodHelper.LoadMenus(-1, false);
+                watch.Stop();
+            }
+            catch (Exception ex)
+            {
+                spamChannel.SendMessageAsync($"Error while fetching food menus: {ex}");
+            }
 
-            var guild = Program.Client.GetGuild(Program.ApplicationSetting.BaseGuild);
-            var spamChannel = guild.GetTextChannel(GeneralChatId);
-            
             // Message only if it took longer than 10 seconds -> some load happened
             //if (spamChannel != null && watch.ElapsedMilliseconds > 10_000)
             //    spamChannel.SendMessageAsync($"Loaded menus in {Math.Round(watch.ElapsedMilliseconds / 1000d, 2)} sec(s)"); // Output in seconds
